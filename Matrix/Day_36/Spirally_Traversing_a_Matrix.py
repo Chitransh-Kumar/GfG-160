4041aa24fd2baba8c55e7d3d074b3367b48764e4
Chitransh Kumar <kumar.chitransh1103@gmail.com>
2025-07-15 09:29:37 +0530

    Add files via upload

---
+++ Matrix/Day_36/Spirally_Traversing_a_Matrix.py
@@ -0,0 +1,34 @@
+def spirally_traversing_a_matrix(matrix):
+    rows=len(matrix)
+    cols=len(matrix[0])
+
+    left=0
+    right=cols-1
+    top=0
+    bottom=rows-1
+
+    ans=[]
+    while top<=bottom and left<=right:
+        
+        for i in range(left,right+1):
+            ans.append(matrix[top][i])
+        top+=1
+
+        for i in range(top,bottom+1):
+            ans.append(matrix[i][right])
+        right-=1
+
+        if top<=bottom:
+            for i in range(right,left-1,-1):
+                ans.append(matrix[bottom][i])
+            bottom-=1
+
+        if left<=right:
+            for i in range(bottom,top-1,-1):
+                ans.append(matrix[i][left])
+            left+=1
+
+    return ans
+
+matrix=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
+print(spirally_traversing_a_matrix(matrix))
